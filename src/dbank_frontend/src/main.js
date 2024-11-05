7d85392ecf6034adc70856159cc9cd4cc934607d
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-11-06 01:17:00 +0530

    style: Design the home page

--- src/dbank_frontend/src/main.js
+++ src/dbank_frontend/src/main.js
@@ -1,4 +1,9 @@
 import App from './App';
+import {dbank_backend} from '../../declarations/dbank_backend'
 import './index.scss';
+
+window.addEventListener('load', () => {
+    console.log("Finished loading...");
+});
 
 const app = new App();

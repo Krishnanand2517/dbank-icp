d585e8bd31a55246525b3b87b37a6434bb0b943e
Krishnanand Yadav <krishnanand2517@gmail.com>
2024-11-06 01:32:01 +0530

    feat: Implement checkBalance and topUp in the frontend

--- src/dbank_frontend/src/main.js
+++ src/dbank_frontend/src/main.js
@@ -2,8 +2,28 @@ import App from './App';
 import {dbank_backend} from '../../declarations/dbank_backend'
 import './index.scss';
 
-window.addEventListener('load', () => {
-    console.log("Finished loading...");
+window.addEventListener('load', async () => {
+    const currentAmount = await dbank_backend.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
 });
+
+document.querySelector("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const button = event.target.querySelector("#submit-btn");
+
+    const inputAmount = parseFloat(document.getElementById("input-amount").value);
+    const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+    button.setAttribute("disabled", true);
+
+    await dbank_backend.topUp(inputAmount);
+
+    const currentAmount = await dbank_backend.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+
+    document.getElementById("input-amount").value = "";
+    button.removeAttribute("disabled");
+})
 
 const app = new App();
